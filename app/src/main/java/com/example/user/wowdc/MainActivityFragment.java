860ee0c9b23b03f94a03d2427eacc877a2bf747c
David <46406163y@iespoblenou.org>
2017-01-14 14:20:46 +0100

    Creacio del Menu refresh

--- app/src/main/java/com/example/user/wowdc/MainActivityFragment.java
+++ app/src/main/java/com/example/user/wowdc/MainActivityFragment.java
@@ -9,6 +9,10 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivityFragment extends Fragment {
 
@@ -17,6 +21,14 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,5 +55,28 @@ public class MainActivityFragment extends Fragment {
         lvWOW.setAdapter(adapter);
 
         return view;
+    }
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_wow_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
     }
 }
